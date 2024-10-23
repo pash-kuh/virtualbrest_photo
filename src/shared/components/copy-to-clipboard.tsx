17e12e3759052a7ae78893fc93e8a5e88d60fb4d
pavel.kuharchik <pavel@chemwatch.com>
2024-10-23 23:25:23 +0300

    Revert "Revert "Revert "test get images"""
    This reverts commit 042a4769ce40e3f0f23e1c6b9e64472db1a2013a.

--- src/shared/components/copy-to-clipboard.tsx
+++ src/shared/components/copy-to-clipboard.tsx
@@ -23,7 +23,7 @@ export const CopyToClipboardButton = ({ text }: { text: string }) => {
             </IconButton>
             <Snackbar
                 message="Copied to clibboard"
-                anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                anchorOrigin={{ vertical: "top", horizontal: "right" }}
                 autoHideDuration={2000}
                 onClose={() => setOpen(false)}
                 open={open}
